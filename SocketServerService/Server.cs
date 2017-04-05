7eb3db5943d187b4ef828af811695153e18954b9
Drummer01 <self1995@mail.ua>
2017-04-05 18:09:19 +0300

    Implemented Server start event

--- SocketServerService/Server.cs
+++ SocketServerService/Server.cs
@@ -1,13 +1,45 @@
 ﻿using System;
 using RemoteControl.Core;
+using Server.Sock;
+using Server.Sock.Core;
+using Fleck;
 
 namespace SocketServerService
 {
     internal static class Server
     {
+        static Chat chat;
+        static Listener listener;
+        static WebSocketServer server;
+        static string runningIp = "not specified";
+
         internal static void onStart(IObervable sender)
         {
+            if (chat != null)
+            {
+                sender.Notify("Server already started");
+                return;
+            }
+            chat = Chat.getInstance();
 
+            listener = Listener.getInstance();
+
+            listener
+                .registerHandler(new Server.Sock.Handlers.Channel())
+                .registerHandler(new Message());
+
+            runningIp = $"ws://{GetIP()}:2000";
+            server = new WebSocketServer(runningIp);
+
+            server.Start(socket =>
+            {
+                socket.OnOpen = () => chat.OnOpen(socket);
+                socket.OnClose = () => chat.OnClose(socket);
+                socket.OnMessage = message => chat.OnMessage(socket, message);
+                socket.OnError = e => Debug.WriteLine(e);
+            });
+
+            sender.Notify("Server started");
         }
 
         internal static void onStop(IObervable sender)
