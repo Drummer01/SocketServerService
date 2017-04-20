cda3cbd8c1e0c2f341e2603e3da8bee7e0f16b22
Drummer01 <self1995@mail.ua>
2017-04-20 18:52:50 +0300

    Exceptions save to file

--- Server/Helpers/ExceptionsLogs.cs
+++ Server/Helpers/ExceptionsLogs.cs
@@ -6,16 +6,36 @@ namespace SocketServer.Helpers
 {
     public static class ExceptionsLogs
     {
-        private static List<Exception> exceptions = new List<Exception>();
+        private const string LOGS_PATH = @"C:\";
+
+        private static List<TimestampException> exceptions = new List<TimestampException>();
 
         public static void Add(Exception e)
         {
-            exceptions.Add(e);
+            exceptions.Add(new TimestampException() { Exception = e });
         }
 
         public static IEnumerable<Exception> Get(int start, int count)
         {
-            return exceptions.Skip(Math.Max(0, exceptions.Count - (count + start)));
+            return exceptions.Skip(Math.Max(0, exceptions.Count - (count + start))).Select( te => te.Exception );
+        }
+
+        public static string SaveToFile()
+        {
+            string fileName = LOGS_PATH + "SocketServerService_logs_" + DateTime.Now + ".log";
+            System.IO.File.WriteAllLines(fileName, exceptions.Select( e => formatException(e) ).ToArray());
+            return fileName;
+        }
+
+        private static string formatException(TimestampException e)
+        {
+            return string.Format("{0} | {1}", e.Time, e.Exception.Message);
+        }
+
+        private class TimestampException
+        {
+            internal Exception Exception { get; set; }
+            internal DateTime Time = DateTime.Now;
         }
     }
 }
